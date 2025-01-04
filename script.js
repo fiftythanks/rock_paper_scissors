f76bc9d0e73dede903aa8da3830f5f9ef09d1b37
fiftythanks <mikhail.sholokhov@tutamail.com>
2025-01-04 14:40:41 +0700

    Create function getComputerChoice()
    getComputerChoice() randomly chooses rock, paper or scissors and
    returns the value of the choice.

---
+++ script.js
@@ -0,0 +1,8 @@
+let pick = ["rock", "paper", "scissors"];
+
+// The computer gives a random choice between rock, paper and scissors
+function getComputerChoice() {
+  return pick[Math.floor(Math.random() * 3)];
+}
+
+// The player picks rock, paper or scissors 
