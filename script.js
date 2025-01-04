3bc39fac78443e105d5b1946ebe73a4b288976ff
fiftythanks <mikhail.sholokhov@tutamail.com>
2025-01-04 15:38:38 +0700

    Create 1/3 of single round logic
    • Declared and initialized to 0 score variables humanScore and
    computerScore.
    • Defined the function playRound() and wrote the first part of the
    function body that defines what happens if the user picks rock and
    the computer picks paper or scissors.

--- script.js
+++ script.js
@@ -9,3 +9,30 @@ function getComputerChoice() {
 function getHumanChoice() {
   return prompt("Rock, paper, scissors?").toLowerCase();
 } 
+
+// Declare the players score variables
+let humanScore = 0;
+let computerScore = 0;
+
+// Single round logic
+// Rock beats scissors, paper beats rock, scissors beat paper
+function playRound(humanChoice, computerChoice) {
+  // User picks rock
+  if (humanChoice === "rock") {
+    if (computerChoice === "scissors") { 
+      humanScore++;
+      console.log("You win! The other player chose scissors. Rock beats scissors.")
+      return `User: ${humanScore}, computer: ${computerScore}`;
+    } else if (computerChoice === "paper") {
+      computerScore++;
+      console.log("You lose! The other player chose paper. Paper beats rock.")
+      return `User: ${humanScore}, computer: ${computerScore}`;
+    }
+  }
+
+  // User picks paper
+
+  // User picks scissors
+
+  // User picks the same thing the computer picked
+}
