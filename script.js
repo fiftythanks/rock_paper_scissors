89ec96e54e113cc993b761c2b3daf07422de49d1
fiftythanks <mikhail.sholokhov@tutamail.com>
2025-01-04 21:07:59 +0700

    Make most of the whole game logic

--- script.js
+++ script.js
@@ -11,9 +11,11 @@ function getHumanChoice() {
 } 
 
 // Declare the players score variables
-let humanScore = 0;
-let computerScore = 0;
-let cont;
+let humanScore;
+let computerScore;
+
+// Declare the desire to do another round variable
+let cont = "y";
 
 // Single round logic
 // Rock beats scissors, paper beats rock, scissors beat paper
@@ -54,4 +56,40 @@ function playRound(humanChoice, computerChoice) {
 
   // Let user decide if he/she wants to play another round
   return cont = prompt("Do you want to play another round? Type y or yes for yes and anything else for no.").toLowerCase();
+}
+
+// The whole game logic
+console.log("This is a simple Rock Paper Scissors game. You will play against your computer. Each round, you'll have to choose rock, paper or scissors. The computer will choose something too. If you win, you get one point. If the computer wins, the computer gets the point. There supposed to be five rounds and if you decide to stop playing before the fifth round, you'll lose.")
+
+
+let start = prompt("Type n to not play the game. Type anything else to start the game.");
+
+if (start !== "n") {
+  let games = 1;
+  let rounds;
+  humanScore = 0;
+  computerScore = 0;
+  let fair_results = [];
+  let results = [];
+
+  while (start !== "n") {
+    rounds = 0;
+
+    while(cont === "y" && rounds <= 5) {
+      if (rounds === 5) {
+        console.log("This game is supposed to be five-round. This is the fifth round, supposedly the last. But if you want to play extra rounds, just continue answering yes to the question at the end of rounds.");
+      }
+      rounds++;
+      playRound(getHumanChoice(), getComputerChoice());
+
+      if (rounds === 5) {
+        fair_results.push([humanScore, computerScore])
+      }
+    }
+
+    // Results
+    if (rounds < 5) {
+      console.log("You lose the game because you quit.")
+    }
+  }
 }
