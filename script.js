c5c06bcca19ee1fb47c914324cb772692ea6c94b
fiftythanks <mikhail.sholokhov@tutamail.com>
2025-01-04 22:06:58 +0700

    Finish the game (no debugging)

--- script.js
+++ script.js
@@ -65,17 +65,22 @@ console.log("This is a simple Rock Paper Scissors game. You will play against yo
 let start = prompt("Type n to not play the game. Type anything else to start the game.");
 
 if (start !== "n") {
-  let games = 1;
+  let games = 0;
+  let fairWins = 0;
+  let wins = 0;
   let rounds;
-  humanScore = 0;
-  computerScore = 0;
-  let fair_results = [];
+  let fairResults = [];
   let results = [];
 
+  // The whole game
   while (start !== "n") {
+    games++;
     rounds = 0;
+    humanScore = 0;
+    computerScore = 0;
 
-    while(cont === "y" && rounds <= 5) {
+    // One game
+    while(cont === "y") {
       if (rounds === 5) {
         console.log("This game is supposed to be five-round. This is the fifth round, supposedly the last. But if you want to play extra rounds, just continue answering yes to the question at the end of rounds.");
       }
@@ -83,13 +88,55 @@ if (start !== "n") {
       playRound(getHumanChoice(), getComputerChoice());
 
       if (rounds === 5) {
-        fair_results.push([humanScore, computerScore])
+        fairResults.push([humanScore, computerScore]);
       }
     }
+    results.push([humanScore, computerScore]); 
 
-    // Results
+    // One game results
     if (rounds < 5) {
-      console.log("You lose the game because you quit.")
+      console.log("You lose the game because you quit.");
+      fairResults.push([humanScore, computerScore]);
+      start = prompt("Do you want to play one more game?");
+    } else {
+      // if humanScore was greater than computerScore by the fifth round
+      if (fairResults[games - 1][0] > fairResults[games - 1][1]) { 
+        fairWins++;
+        wins++;
+        console.log(`You win game ${games} fairly. Congratulations!`);
+
+      // if computerScore was greater than humanScore by the fifth round but humanScore is greater than computerScore now
+      } else if (fairResults[games - 1][1] > fairResults[games - 1][0] && humanScore > computerScore) {
+        console.log(`You lose game ${games} fairly, but you win if you count the extra rounds.`);
+        wins++;
+
+      // if computerScore was and is greater in both cases
+      } else if (fairResults[games - 1][1] > fairResults[games - 1][0] && humanScore < computerScore) {
+        console.log(`You lose game ${games}.`);
+      
+      // if the scores were and are equal
+      } else if (fairResults[games - 1][0] === fairResults[games - 1][1] && humanScore === computerScore) {
+        console.log(`Game ${games} is a draw.`);
+
+      // if the scores were equal but aren't now
+      } else if (fairResults[games - 1][0] === fairResults[games - 1][1] && humanScore !== computerScore) {
+        if (humanScore > computerScore) {
+          console.log(`Fairly, game ${games} is a draw. But if you count the extra rounds, you win.`);
+          wins++;
+        } else if (computerScore) {
+          console.log(`Fairly, game ${games} is a draw. But if you count the extra rounds, you lose.`);
+        }
+      }
+
+      start = prompt("Do you want to play one more game?");
     }
   }
+
+  console.log("You finished your game. The results:");
+  console.log(`Fair wins: ${fairWins}\n Wins: ${wins}`);
+  for(let i = 0; i < games; i++) {
+    console.log(`Game ${i + 1}: ${fairResults[i][0]}-${fairResults[i][1]} fairly, ${results[i][0]}-${results[i][1]} counting extra rounds`);
+  }
+} else {
+  console.log("You declined the offer to play the game.");
 }
